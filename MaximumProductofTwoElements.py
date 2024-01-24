7aa57384f14a93c02346fd046320fc8314ca46f9
Manish Yadav <45384589+Manish-gituser@users.noreply.github.com>
2024-01-24 22:20:55 +0530

    Add files via upload

---
+++ MaximumProductofTwoElements.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxProduct(self, nums: List[int]) -> int:
+        l=len(nums)
+        # res=0
+        # nums.sort()
+        # return((nums[-1]-1)*(nums[-2]-1))
+
+        maxi=0
+        second=0
+        counter=0
+
+        for i in range(l):
+            # second=maxi
+            if(nums[i]>=maxi):
+                counter=maxi
+                maxi=nums[i]
+               
+                continue
+
+            elif (nums[i]>counter):
+                    counter = nums[i]
+                
+
+                
+
+            
+        #print(maxi,counter)    
+        return ((maxi-1) * (counter-1))    
+
+
+
+
+        
